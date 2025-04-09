f3bcdf69331673852ad97ca00032c87d454ca45c
Cesar Pires <cesar.augusto.pires@hotmail.com>
2025-04-08 21:39:40 -0300

    feat: replace SideBarOption with SidebarContent for improved sidebar navigation

--- frontend/src/app/map/page.tsx
+++ frontend/src/app/map/page.tsx
@@ -1,7 +1,7 @@
 import { Button } from "@/components/ui/button";
 import Header from "@/components/ui/header/hearder";
 import SelectLocation from "@/components/ui/selects/SelectLocation";
-import SideBarOption from "@/components/ui/sidebar/sidebar-button";
+import SidebarContent from "@/components/ui/sidebar/sidebar-content";
 import { Plus } from "@mynaui/icons-react";
 
 const locations = [
@@ -10,27 +10,11 @@ const locations = [
   "Fazenda Laranjal",
 ];
 
-const options = [
-  { name: "🔍 Mapa de pulverizações", link: "/map" },
-  { name: "📅 Calendário de pulverizações", link: "/schedule" },
-  { name: "⚙️ Meus locais", link: "/my-places" },
-];
-
 export default function MapaPulverizacoes() {
   return (
     <div className="bg-green-100 min-h-screen flex">
       {/* Sidebar */}
-      <div className="w-1/4 bg-white shadow-md p-4">
-        <div className="bg-orange-400 text-white font-bold text-lg p-3 rounded-md flex items-center">
-          <div className="w-8 h-8 bg-gray-200 rounded-full flex items-center justify-center mr-2 text-lg">
-            T
-          </div>
-          T.R.E.M.
-        </div>
-        <nav className="mt-4 space-y-2">
-          <SideBarOption options={options} />
-        </nav>
-      </div>
+      <SidebarContent />
 
       {/* Conteúdo Principal */}
       <div className="w-3/4 p-4">
--- frontend/src/app/my-places/page.tsx
+++ frontend/src/app/my-places/page.tsx
@@ -1,5 +1,5 @@
 import Header from "@/components/ui/header/hearder";
-import SideBarOption from "@/components/ui/sidebar/sidebar-button";
+import SidebarContent from "@/components/ui/sidebar/sidebar-content";
 
 const locations = [
   "Minha localização",
@@ -7,27 +7,10 @@ const locations = [
   "Fazenda Laranjal",
 ];
 
-const options = [
-  { name: "🔍 Mapa de pulverizações", link: "/map" },
-  { name: "📅 Calendário de pulverizações", link: "/schedule" },
-  { name: "⚙️ Meus locais", link: "/my-places" },
-];
-
 export default function MapaPulverizacoes() {
   return (
     <div className="bg-green-100 min-h-screen flex">
-      {/* Sidebar */}
-      <div className="w-1/4 bg-white shadow-md p-4">
-        <div className="bg-orange-400 text-white font-bold text-lg p-3 rounded-md flex items-center">
-          <div className="w-8 h-8 bg-gray-200 rounded-full flex items-center justify-center mr-2 text-lg">
-            T
-          </div>
-          T.R.E.M.
-        </div>
-        <nav className="mt-4 space-y-2">
-          <SideBarOption options={options} />
-        </nav>
-      </div>
+      <SidebarContent />
 
       {/* Conteúdo Principal */}
       <div className="w-3/4 p-4">
