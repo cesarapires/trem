e87a72873c71f7bdc4e7c2aa36645a4b41924713
Cesar Pires <cesar.augusto.pires@hotmail.com>
2025-04-21 15:21:38 -0300

    chore: updates type definitions for coordinates in store

--- frontend/src/store/property-store.ts
+++ frontend/src/store/property-store.ts
@@ -2,9 +2,9 @@ import { create } from "zustand";
 
 type PropertyStore = {
   property: string | null;
-  coordinates: [number, number] | null;
+  coordinates: [Number, Number] | null;
   setProperty: (value: string) => void;
-  setCoordinates: (value: [number, number]) => void;
+  setCoordinates: (value: [Number, Number]) => void;
 };
 
 export const usePropertyStore = create<PropertyStore>((set) => ({
