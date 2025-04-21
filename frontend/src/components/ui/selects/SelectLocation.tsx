dea755d6bb6d751c6fed2d174cffac1db6c270ac
Cesar Pires <cesar.augusto.pires@hotmail.com>
2025-04-21 12:52:22 -0300

    feat: integrate property store for location selection in SelectLocation component

--- frontend/src/components/ui/selects/SelectLocation.tsx
+++ frontend/src/components/ui/selects/SelectLocation.tsx
@@ -1,20 +1,30 @@
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
+"use client";
 
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { usePropertyStore } from "@/store/property-store";
 
 interface SelectLocationProps {
-  locations: string[];
+  locations: {id: string, name: string}[];
 }
 
 const SelectLocation: React.FC<SelectLocationProps> = ({ locations }) => {
+  const { setProperty } = usePropertyStore();
+
   return (
-    <Select>
+    <Select onValueChange={(value) => setProperty(value)}>
       <SelectTrigger className="w-full sm:w-[180px] md:w-[300px] lg:w-[350px] bg-white text-black border border-gray-300">
         <SelectValue placeholder={"Escolha uma localização"} />
       </SelectTrigger>
       <SelectContent>
         {locations.map((location, index) => (
-          <SelectItem key={index} value={location}>
-            {location}
+          <SelectItem key={index} value={location.id}>
+            {location.name}
           </SelectItem>
         ))}
       </SelectContent>
