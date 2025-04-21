202f23777e7b8486e2f34cd052ff0e1374312091
Cesar Pires <cesar.augusto.pires@hotmail.com>
2025-04-21 12:52:18 -0300

    feat: add NewPulverizationPopover component for creating new pulverizations

---
+++ frontend/src/components/ui/popovers/NewPulverizationPopover.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { Button } from "../button";
+import { Plus } from "@mynaui/icons-react";
+import { CalendarForm } from "../calendar/PulverizationCalendar";
+
+const NewPulverizationPopover: React.FC = () => {
+  return (
+    <Popover>
+      <PopoverTrigger asChild>
+        <Button variant="secondary" className="cursor-pointer">
+          <Plus /> Nova pulverização
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="w-80">
+        <CalendarForm />
+      </PopoverContent>
+    </Popover>
+  );
+};
+
+export default NewPulverizationPopover;
