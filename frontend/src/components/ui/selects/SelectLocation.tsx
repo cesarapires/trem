476ef5735c1f61953d1d9e6cccf794a574b281e6
Cesar Pires <cesar.augusto.pires@hotmail.com>
2025-04-21 15:23:19 -0300

    feat: enhances location selection functionality

--- frontend/src/components/ui/selects/SelectLocation.tsx
+++ frontend/src/components/ui/selects/SelectLocation.tsx
@@ -10,20 +10,40 @@ import {
 import { usePropertyStore } from "@/store/property-store";
 
 interface SelectLocationProps {
-  locations: {id: string, name: string}[];
+  locations: {
+    id: string;
+    name: string;
+    coordinates: { latitude: Number; longitude: Number };
+  }[];
 }
 
 const SelectLocation: React.FC<SelectLocationProps> = ({ locations }) => {
-  const { setProperty } = usePropertyStore();
+  const { setProperty, setCoordinates } = usePropertyStore();
+
+  const handleChange = (value: string) => {
+    const selectedLocation = locations.find((location) => location.id === value);
+
+    const latitude = selectedLocation?.coordinates.latitude ?? 0;
+    const longitude = selectedLocation?.coordinates.longitude ?? 0;
+
+    if (selectedLocation) {
+      setProperty(selectedLocation.id);
+      setCoordinates([latitude, longitude]);
+    }
+  };
 
   return (
-    <Select onValueChange={(value) => setProperty(value)}>
+    <Select onValueChange={handleChange}>
       <SelectTrigger className="w-full sm:w-[180px] md:w-[300px] lg:w-[350px] bg-white text-black border border-gray-300">
         <SelectValue placeholder={"Escolha uma localização"} />
       </SelectTrigger>
       <SelectContent>
         {locations.map((location, index) => (
-          <SelectItem key={index} value={location.id}>
+          <SelectItem
+            key={index}
+            data-coordinates={location.coordinates}
+            value={location.id}
+          >
             {location.name}
           </SelectItem>
         ))}
