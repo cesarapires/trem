66897bebdb50231db55fa3caba906de2a031c0eb
Cesar Pires <cesar.augusto.pires@hotmail.com>
2025-04-21 12:58:14 -0300

    feat: replaces hardcoded location IDs with unique UUIDs for better data integrity and identification.

--- frontend/src/app/(user-area)/map/@header/page.tsx
+++ frontend/src/app/(user-area)/map/@header/page.tsx
@@ -3,9 +3,9 @@ import NewPulverizationPopover from "@/components/ui/popovers/NewPulverizationPo
 import SelectLocation from "@/components/ui/selects/SelectLocation";
 
 const locations = [
-  { id: "1", name: "Minha localização" },
-  { id: "2", name: "Fazenda Sol Nascente" },
-  { id: "3", name: "Fazenda Laranjal" },
+  { id: "399821bb-1db2-40ac-9705-7507681d47dc", name: "Minha localização" },
+  { id: "b8389d16-3e43-4207-a02f-45f821b39042", name: "Fazenda Sol Nascente" },
+  { id: "190cf364-9e35-4ce6-bc48-e7a0821a6db2", name: "Fazenda Laranjal" },
 ];
 
 export default function MapHeader() {
