dba8481678378d812d398108f7849b9a6aba1429
Cesar Pires <cesar.augusto.pires@hotmail.com>
2025-04-21 15:24:17 -0300

    feat: updates location coordinates in header components

--- frontend/src/app/(user-area)/schedule/@header/page.tsx
+++ frontend/src/app/(user-area)/schedule/@header/page.tsx
@@ -6,24 +6,24 @@ const locations = [
     id: "399821bb-1db2-40ac-9705-7507681d47dc",
     name: "Minha localização",
     coordinates: {
-      latitude: -23.55052,
-      longitude: -46.633308,
+      latitude: -20.899349709981426,
+      longitude: -46.99061049779196,
     },
   },
   {
     id: "b8389d16-3e43-4207-a02f-45f821b39042",
     name: "Fazenda Sol Nascente",
     coordinates: {
-      latitude: -22.90556,
-      longitude: -47.06083,
+      latitude: -21.232089043346647,
+      longitude: -44.99445408674582,
     },
   },
   {
     id: "190cf364-9e35-4ce6-bc48-e7a0821a6db2",
     name: "Fazenda Laranjal",
     coordinates: {
-      latitude: -20.3155,
-      longitude: -40.3128,
+      latitude: -21.161141367126774,
+      longitude: -44.92431764431109,
     },
   },
 ];
