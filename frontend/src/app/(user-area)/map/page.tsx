003fd98214e850d736b5316981320dc411c6531e
Cesar Pires <cesar.augusto.pires@hotmail.com>
2025-04-21 15:43:04 -0300

    feat: implements interactive map feature using Leaflet

--- frontend/src/app/(user-area)/map/page.tsx
+++ frontend/src/app/(user-area)/map/page.tsx
@@ -1,13 +1,34 @@
-import Image from "next/image";
+"use client";
+
+import { CenterMap } from "@/components/ui/map/CenterMap";
+import { usePropertyStore } from "@/store/property-store";
+import "leaflet/dist/leaflet.css";
+import { MapContainer, TileLayer, Popup, Marker } from "react-leaflet";
 
 export default function MapaPulverizacoes() {
+  const { coordinates } = usePropertyStore();
+
   return (
-    <Image
-      src="/map-placeholder.jpeg"
-      alt="Mapa temporário"
-      className="w-full h-96 object-cover rounded-md"
-      width={800}
-      height={400}
-    />
+    <div className="relative h-[500px] w-full z-0">
+      <MapContainer
+        center={coordinates ?? [0, 0]}
+        zoom={13}
+        scrollWheelZoom={false}
+        style={{ height: "100%", width: "100%" }}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        {coordinates && (
+          <>
+            <Marker position={coordinates}>
+              <Popup>Local selecionado</Popup>
+            </Marker>
+            <CenterMap coordinates={coordinates} />
+          </>
+        )}
+      </MapContainer>
+    </div>
   );
 }
