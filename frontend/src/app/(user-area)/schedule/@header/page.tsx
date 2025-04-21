92198b0a2aae82e32afad10e32e2d62b0eda9a59
Cesar Pires <cesar.augusto.pires@hotmail.com>
2025-04-21 13:00:49 -0300

    feat: adds coordinates to location objects in headers

--- frontend/src/app/(user-area)/schedule/@header/page.tsx
+++ frontend/src/app/(user-area)/schedule/@header/page.tsx
@@ -2,9 +2,30 @@ import Header from "@/components/ui/header/hearder";
 import SelectLocation from "@/components/ui/selects/SelectLocation";
 
 const locations = [
-  { id: "399821bb-1db2-40ac-9705-7507681d47dc", name: "Minha localização" },
-  { id: "b8389d16-3e43-4207-a02f-45f821b39042", name: "Fazenda Sol Nascente" },
-  { id: "190cf364-9e35-4ce6-bc48-e7a0821a6db2", name: "Fazenda Laranjal" },
+  {
+    id: "399821bb-1db2-40ac-9705-7507681d47dc",
+    name: "Minha localização",
+    coordinates: {
+      latitude: -23.55052,
+      longitude: -46.633308,
+    },
+  },
+  {
+    id: "b8389d16-3e43-4207-a02f-45f821b39042",
+    name: "Fazenda Sol Nascente",
+    coordinates: {
+      latitude: -22.90556,
+      longitude: -47.06083,
+    },
+  },
+  {
+    id: "190cf364-9e35-4ce6-bc48-e7a0821a6db2",
+    name: "Fazenda Laranjal",
+    coordinates: {
+      latitude: -20.3155,
+      longitude: -40.3128,
+    },
+  },
 ];
 
 export default function ScheduleHeader() {
