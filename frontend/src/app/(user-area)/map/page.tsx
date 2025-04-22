a13a4a1a1a0c71e9b456de6bbd453d9f77642a05
Cesar Pires <cesar.augusto.pires@hotmail.com>
2025-04-22 10:07:49 -0300

    chore: refactors map component to use dynamic loading

--- frontend/src/app/(user-area)/map/page.tsx
+++ frontend/src/app/(user-area)/map/page.tsx
@@ -1,34 +1,15 @@
 "use client";
 
-import { CenterMap } from "@/components/ui/map/CenterMap";
-import { usePropertyStore } from "@/store/property-store";
+import dynamic from "next/dynamic"
+
 import "leaflet/dist/leaflet.css";
-import { MapContainer, TileLayer, Popup, Marker } from "react-leaflet";
+
+const MapLayout = dynamic(() => import("@/components/ui/map/Map"), { ssr:false })
 
 export default function MapaPulverizacoes() {
-  const { coordinates } = usePropertyStore();
-
   return (
     <div className="relative h-[500px] w-full z-0">
-      <MapContainer
-        center={coordinates ?? [0, 0]}
-        zoom={13}
-        scrollWheelZoom={false}
-        style={{ height: "100%", width: "100%" }}
-      >
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        {coordinates && (
-          <>
-            <Marker position={coordinates}>
-              <Popup>Local selecionado</Popup>
-            </Marker>
-            <CenterMap coordinates={coordinates} />
-          </>
-        )}
-      </MapContainer>
+      <MapLayout />
     </div>
   );
 }
