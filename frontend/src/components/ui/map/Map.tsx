a6df9ae53262dee39ec34c95f5f93756e185e314
Cesar Pires <cesar.augusto.pires@hotmail.com>
2025-04-22 10:07:32 -0300

    feat: adds interactive map component with markers

---
+++ frontend/src/components/ui/map/Map.tsx
@@ -0,0 +1,31 @@
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import { CenterMap } from "./CenterMap";
+import { usePropertyStore } from "@/store/property-store";
+
+export default function Map() {
+  const { coordinates } = usePropertyStore();
+
+  return (
+    <div className="relative h-[500px] w-full z-0">
+      <MapContainer
+        center={coordinates ?? [0, 0]}
+        zoom={13}
+        scrollWheelZoom={false}
+        style={{ height: "100%", width: "100%" }}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        {coordinates && (
+          <>
+            <Marker position={coordinates}>
+              <Popup>Local selecionado</Popup>
+            </Marker>
+            <CenterMap coordinates={coordinates} />
+          </>
+        )}
+      </MapContainer>
+    </div>
+  );
+}
