840785f9a3cbb96608b99569466af1b274fa2f1f
Cesar Pires <cesar.augusto.pires@hotmail.com>
2025-05-20 23:04:39 -0300

    refactor: ajusta o cabeçalho para permitir truncamento do título e melhorar a responsividade

--- frontend/src/components/ui/header/hearder.tsx
+++ frontend/src/components/ui/header/hearder.tsx
@@ -5,8 +5,10 @@ interface HeaderProps {
 
 const Header: React.FC<HeaderProps> = ({ title, children }) => {
   return (
-    <div className="h-16 bg-orange-400 px-3 rounded-md flex justify-between items-center text-white mb-4">
-      <h2 className="text-xl font-semibold tracking-tight">{title}</h2>
+    <div className="h-16 bg-orange-400 px-3 rounded-md flex justify-between items-center text-white mb-4 overflow-hidden">
+      <h2 className="text-base sm:text-xl font-semibold tracking-tight truncate max-w-[70%]">
+        {title}
+      </h2>
       {children && <div className="flex space-x-2">{children}</div>}
     </div>
   );
