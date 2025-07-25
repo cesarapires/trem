a479de4a77a63e84500fc37069ce29230287d985
Cesar Pires <cesar.augusto.pires@hotmail.com>
2025-07-24 21:02:15 -0300

    feat: adiciona página de login e componente de formulário de login

---
+++ frontend/src/app/login/page.tsx
@@ -0,0 +1,30 @@
+import { LoginForm } from "@/components/ui/login-form";
+
+export default function LoginPage() {
+  return (
+    <div className="grid min-h-svh lg:grid-cols-2">
+      <div className="flex flex-col gap-4 p-6 md:p-10">
+        <div className="flex justify-center gap-2 md:justify-start">
+          <div className="font-bold text-lg p-3 rounded-md flex items-center">
+            <div className="w-8 h-8 bg-gray-200 rounded-full flex items-center justify-center mr-2 text-lg">
+              T
+            </div>
+            T.R.E.M.
+          </div>
+        </div>
+        <div className="flex flex-1 items-center justify-center">
+          <div className="w-full max-w-xs">
+            <LoginForm />
+          </div>
+        </div>
+      </div>
+      <div className="bg-muted relative hidden lg:block">
+        <img
+          src="/login-banner.png"
+          alt="Image"
+          className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+        />
+      </div>
+    </div>
+  );
+}
