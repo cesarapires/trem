09cd1e0e1357f4e5b5e7af19b4bdd3573f7cfb1f
Cesar Pires <cesar.augusto.pires@hotmail.com>
2025-10-19 21:35:30 -0300

    feat: adiciona ícone de fazenda e atualiza ícone de trator com suporte a múltiplos tipos

--- frontend/src/components/ui/map/Map.tsx
+++ frontend/src/components/ui/map/Map.tsx
@@ -2,6 +2,8 @@ import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 import { CenterMap } from "./CenterMap";
 import { usePropertyStore } from "@/store/property-store";
 import TractorIcon from "./TractorIcon";
+import { FrameIcon } from "lucide-react";
+import FarmIcon from "./FarmIcon";
 
 export default function Map() {
   const { coordinates } = usePropertyStore();
@@ -18,9 +20,12 @@ export default function Map() {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
+        <Marker key={1} position={[-21.20, -44.90]} icon={TractorIcon({type: "safe"})}>
+          <Popup>Local selecionado</Popup>
+        </Marker>
         {coordinates && (
           <>
-            <Marker position={coordinates} icon={TractorIcon}>
+            <Marker key={0} position={coordinates} icon={FarmIcon}>
               <Popup>Local selecionado</Popup>
             </Marker>
             <CenterMap coordinates={coordinates} />
