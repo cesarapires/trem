6f8d6bef26aad6330650d39546aa8a3e08dcc90f
Cesar Pires <cesar.augusto.pires@hotmail.com>
2025-10-21 23:19:27 -0300

    feat: adiciona configuração do MongoDB e DTOs para agendamento de aplicações

--- backend/trem-gis/src/app.module.ts
+++ backend/trem-gis/src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ApplicationScheduleModule } from './application-schedule/application-schedule.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    ApplicationScheduleModule,
+    MongooseModule.forRoot('mongodb://localhost:27017/your-database-name'),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
