9a70c5f1b584a93f1997f6a5a763d2d7ee4286e7
Cesar Pires <cesar.augusto.pires@hotmail.com>
2025-11-21 11:14:03 -0300

    feat: refatora componentes de propriedades e adiciona geração de dados fictícios

--- frontend/src/app/(user-area)/my-places/page.tsx
+++ frontend/src/app/(user-area)/my-places/page.tsx
@@ -1,15 +1,11 @@
-"use client";
+"use client"
 
-import dynamic from "next/dynamic"
+import { PropertyList } from '@/components/property/PropertyList'
 
-import "leaflet/dist/leaflet.css";
-
-const MapLayout = dynamic(() => import("@/components/ui/map/Map"), { ssr:false })
-
-export default function MapaPulverizacoes() {
+export default function MinhasPropriedades() {
   return (
-    <div className="relative h-[calc(100vh-theme(spacing.32))] z-0">
-      <MapLayout />
+    <div className="relative h-[calc(100vh-theme(spacing.32))] z-0 overflow-auto">
+      <PropertyList />
     </div>
-  );
+  )
 }
--- frontend/src/domain/models/property-model.ts
+++ frontend/src/domain/models/property-model.ts
@@ -1,6 +1,19 @@
 export type PropertyModel = {
-  id: string
-  name: string
-  longitude: number
-  latitude: number
+  id: string;
+
+  name: string;
+
+  ownerName?: string;
+
+  ownerCpfCnpj?: string;
+
+  city?: string;
+
+  fullAddress?: string;
+
+  state?: string;
+
+  latitude: number;
+
+  longitude: number;
 }
