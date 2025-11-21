809ff82cc11ea0461e803cd53df004a7138bef26
Cesar Pires <cesar.augusto.pires@hotmail.com>
2025-11-21 10:21:11 -0300

    feat: adiciona fábricas para AxiosHttpClient e RemoteGetAllProperties, incluindo rotas da API

---
+++ frontend/src/main/factories/usecases/remote-get-all-properties-factory.ts
@@ -0,0 +1,8 @@
+import { RemoteGetAllProperties } from '@/data/usecases/remote-get-all-properties'
+import { GetAllProperties } from '@/domain/usecases/get-all-properties'
+import { makeApiUrl } from '../http/axios-http-client-factory'
+import { makeAxiosHttpClient } from '../http/api-url-factory'
+import { apiRoutes } from '@/main/routes/api-routes'
+
+export const makeRemoteGetAllProperties = (): GetAllProperties =>
+  new RemoteGetAllProperties(makeApiUrl(apiRoutes.propertyLean), makeAxiosHttpClient())
