bc2fbee5a79972c9d662b7648511c8dd90f30d50
Cesar Pires <cesar.augusto.pires@hotmail.com>
2025-11-21 02:07:28 -0300

    feat: implementação da classe RemoteGetAllProperties com método getAll

--- frontend/src/data/usecases/remote-get-all-properties.ts
+++ frontend/src/data/usecases/remote-get-all-properties.ts
@@ -1,8 +1,23 @@
 import { PropertyModel } from "@/domain/models/property-model";
 import { GetAllProperties } from "@/domain/usecases/get-all-properties";
+import { HttpGetClient } from "../protocols/http/http-get-client";
+import { HttpStatusCode } from "../protocols/http/http-response";
 
 export class RemoteGetAllProperties implements GetAllProperties {
-  getAll(): Promise<PropertyModel[]> {
-    throw new Error("Method not implemented.");
+
+  constructor (
+    private readonly url: string,
+    private readonly httpGetClient: HttpGetClient<void, PropertyModel[]>
+  ) {}
+
+  async getAll(): Promise<PropertyModel[]> {
+    const httpResponse = await this.httpGetClient.get({
+      url: this.url
+    })
+
+    switch (httpResponse.statusCode) {
+      case HttpStatusCode.ok: return httpResponse.body!
+      default: throw new Error("UnexpectedError")
+    }
   }
 }
